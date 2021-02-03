697f1718018d592fbf61caecf745c5217b556031
UBC Student <alexig18@valdes.students.cs.ubc.ca>
2021-02-03 11:50:29 -0800

    fixed up some stuff and added make file

--- chain.cpp
+++ chain.cpp
@@ -29,22 +29,20 @@ Chain::Node * Chain::insertAfter(Node * p, const Block &ndata) {
   /* your code here */
   Node *newNode = new Node(ndata);
   if (p == NULL) {
-    Node *temp = this->head_;
-    temp->previous = newNode;
-    newNode->next = temp;
-    this->head_= newNode;
-    delete temp;
+    head_->prev = newNode;
+    newNode->next = head_;
+    head_ = newNode;
   } else {
     if (p->next == NULL) {
+      //if p is the tail
       p->next = newNode;
-      newNode->previous = p;
+      newNode->prev = p;
     } else {
       Node *next = p->next;
-      next->previous = newNode;
+      next->prev = newNode;
       newNode->next = next;
-      newNode->previous = p;
+      newNode->prev = p;
       p->next = newNode;
-      delete next;
     }
   }
   length_++;
@@ -58,39 +56,35 @@ Chain::Node * Chain::insertAfter(Node * p, const Block &ndata) {
  * @param second = The node that comes second in the chain.
  */
 void Chain::swapSideBySide(Node *first, Node *second) {
-  if (this->head_ == first) {
+  if (first->prev == NULL) {
+    //if first is the head node
     Node * temp = second->next;
     first->next = temp;
-    temp->previous = first;
-    first->previous = second;
+    temp->prev = first;
+    first->prev = second;
     second->next = first;
-    second->previous = NULL;
-    this->head_ = second;
-    delete temp;
+    second->prev = NULL;
+    head_ = second;
     return;
   } else if (second->next == NULL) {
     //if second is the tail
-    Node *temp = first->previous;
-    second->previous = temp;
+    Node *temp = first->prev;
+    second->prev = temp;
     temp->next = second;
     second->next = first;
-    first->previous = second;
+    first->prev = second;
     first->next = NULL;
-    delete temp;
     return;
   } else {
-    Node *previous_p = p->previous;
-    Node *next_q = q->next;
+    Node *previous_first = first->prev;
+    Node *next_second = second->next;
 
-    previous_p->next = q;
-    q->previous = previous_p;
-    q->next = p;
-    p->previous = q;
-    p->next = next_q;
-    next_q->previous = p;
-
-    delete previous_p;
-    delete next_q;
+    previous_first->next = second;
+    second->prev = previous_first;
+    second->next = first;
+    first->prev = second;
+    first->next = next_second;
+    next_second->prev = first;
     return;
   }
 }
@@ -112,19 +106,19 @@ void Chain::swap(Node *p, Node *q) {
   }
   //if there are only 2 blocks in the chain
   if (length_ == 2) {
-    if (this->head_ == p) {
-      this->head_ = q;
-      q->previous = NULL;
+    if (p->prev == NULL) {
+      head_ = q;
+      q->prev = NULL;
       q->next = p;
       p->next = NULL;
-      p->previous = q;
+      p->prev = q;
       return;
     } else {
-      this->head_ = p;
-      p->previous = NULL;
+      head_ = p;
+      p->prev = NULL;
       p->next = q;
       q->next = NULL;
-      q->previous = p;
+      q->prev = p;
       return;
     }
   }
@@ -137,45 +131,41 @@ void Chain::swap(Node *p, Node *q) {
     return;
   }
   //Otherwise:
-  Node *previous_p = p->previous;
+  Node *previous_p = p->prev;
   Node *next_p = p->next;
-  Node *previous_q = q->previous;
+  Node *previous_q = q->prev;
   Node *next_q = q->next;
 
   if (previous_p == NULL) {
     //This means p was the head
-    q->previous = NULL;
+    q->prev = NULL;
     q->next = next_p;
-    next_p->previous = q; 
-    this->head_ = q;
+    next_p->prev = q; 
+    head_ = q;
   } else {
-    q->previous = previous_p;
+    q->prev = previous_p;
     previous_p->next = q;
     q->next = next_p;
     //if p was not the original tail
     if (next_p != NULL) {
-      next_p->previous = q;
+      next_p->prev = q;
     }
   }
   if (previous_q == NULL) {
     //This means q was the head
-    p->previous = NULL;
+    p->prev = NULL;
     p->next = next_q;
-    next_q->previous = p;
-    this->head_ = p;
+    next_q->prev = p;
+    head_ = p;
   } else {
-    p->previous = previous_q;
+    p->prev = previous_q;
     previous_q->next = p;
     p->next = next_q;
     //if q was not the original tail
     if (next_q != NULL) {
-      next_q->previous = p;
+      next_q->prev = p;
     }
   }
-  delete previous_p;
-  delete next_p;
-  delete previous_q;
-  delete next_q;
 }
 
 /**
@@ -184,7 +174,8 @@ void Chain::swap(Node *p, Node *q) {
  */
 void Chain::clear() {
   /* your code here */
-  delete this->head_;
+  delete head_;
+  head_ = NULL;
 }
 
 /**
@@ -196,22 +187,20 @@ void Chain::clear() {
  */
 void Chain::copy(Chain const &other) {
   /* your code here */
-  this->length_ = other->length_;
-  Node * curr = other->head_;
+  this->length_ = other.length_;
+  Node * curr = other.head_;
   Node * copy = new Node(curr->data);
   this->head_ = copy;
   while (curr != NULL) {
     if (curr->next != NULL) {
       copy->next = new Node(curr->next->data);
     }
-    if (curr->previous != NULL) {
-      copy->previous = new Node(curr->previous->data);
+    if (curr->prev != NULL) {
+      copy->prev = new Node(curr->prev->data);
     }
     copy = copy->next;
     curr = curr->next;
   }
-  delete curr;
-  delete copy;
 }
 
 /* Modifies the current chain: 
